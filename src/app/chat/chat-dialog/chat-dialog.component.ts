906684dee4d3862c76f315d1e99d856828760d8e
Jorge <jorge@Users-MacBook-Pro.local>
2020-06-18 22:47:04 -0600

    test 1 UI

--- src/app/chat/chat-dialog/chat-dialog.component.ts
+++ src/app/chat/chat-dialog/chat-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
 import { ChatService, Message } from '../chat.service';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/scan';
@@ -9,22 +9,41 @@ import 'rxjs/add/operator/scan';
   templateUrl: './chat-dialog.component.html',
   styleUrls: ['./chat-dialog.component.scss']
 })
+
 export class ChatDialogComponent implements OnInit {
 
   messages: Observable<Message[]>;
   formValue: string;
 
-  constructor(public chat: ChatService) { }
-
+  @ViewChild('closeTemplate') closeTemplate: TemplateRef<any>;
+  @ViewChild('openTemplate') openTemplate: TemplateRef<any>;
+  estado: boolean;
+  name = 'Dynamic Temmplates';
+  constructor(public chat: ChatService) {
+    this.estado = true;
+  }
   ngOnInit() {
     // appends to array after each new message is added to feedSource
     this.messages = this.chat.conversation.asObservable()
-        .scan((acc, val) => acc.concat(val) );
+      .scan((acc, val) => acc.concat(val) );
   }
 
   sendMessage() {
     this.chat.converse(this.formValue);
     this.formValue = '';
+  }
+
+  get getTemplate() {
+    if (this.estado) {
+      return this.openTemplate;
+    } else {
+      return this.closeTemplate;
+    }
+
+  }
+
+  changeTemplate() {
+    this.estado = !this.estado;
   }
 
 }
